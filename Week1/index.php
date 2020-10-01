cba9b9f866e8bc32a1c054a46afa1ddeadcccf73
MrQuanggit <vodangquang13579@gmail.com>
2020-10-01 13:47:05 +0700

    Module 2

---
+++ Week1/index.php
@@ -0,0 +1,20 @@
+<?php
+    $info = "PHP C8";
+
+    echo $info
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bài học đầu tiên PHP</title>
+</head>
+<body>
+<?php
+    $info = "PHP C8";
+
+    echo $info
+?>
+</body>
+</html>
